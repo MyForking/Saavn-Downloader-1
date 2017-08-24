a9fcab63ad956ba4ccd651f92fb170a6eb9b208c
naqushab <naqushab.neyazee24@gmail.com>
2017-08-24 19:54:43 +0530

    download to file and check for duplicates

--- saavn_downloader.py
+++ saavn_downloader.py
@@ -50,30 +50,48 @@ def downloader_saavn(input_url):
     # Encrypted url to the mp3 are stored in the webpage
     songs_json = soup.find_all('div',{'class':'hide song-json'})
 
-    linksFile = open('links.html','a+')
-
+    currentDirectory = os.path.join(os.getcwd(), "downloads")
+    
     for song in songs_json:
         obj = json_decoder.decode(song.text)
         print(obj['album'],'-',obj['title'])
         enc_url = base64.b64decode(obj['url'].strip())
         dec_url = des_cipher.decrypt(enc_url,padmode=PAD_PKCS5).decode('utf-8')
         dec_url = base_url + dec_url.replace('mp3:audios','') + '.mp3'
-        print(dec_url,'\n')
-        linksFile.write("<a href=\"" + dec_url + "\">" + obj['title'] + "<a> <br>")
-        wget.download(dec_url, obj['title']+".mp3")
+        #print(dec_url,'\n')
+        fileExistsName = os.path.join(currentDirectory, obj['title']+".mp3")
+        if os.path.exists(fileExistsName):
+            continue
+        else:
+            #wget.download(dec_url, obj['title']+".mp3")
+            pass
+
+    for fileName in os.listdir(os.getcwd()):
+        if fileName.endswith(".mp3"):
+            sourceFile = os.path.join(os.getcwd(), fileName)
+            destinationFile = os.path.join(currentDirectory, fileName)
+            os.rename(sourceFile, destinationFile)
 
 if __name__ == "__main__":
-    input_url = input('Enter the song url:').strip()
+    loop = True
+    while(loop):
+        input_url = input('Enter the song url:').strip()
 
-    if 'search' in input_url:
-        try:
-            res = requests.get(input_url, proxies=proxies, headers=headers)
-        except Exception as e:
-            print('Error accessing website error: '+e)
-            sys.exit()
+        if 'search' in input_url:
+            try:
+                res = requests.get(input_url, proxies=proxies, headers=headers)
+            except Exception as e:
+                print('Error accessing website error: '+e)
+                sys.exit()
 
-        soup = BeautifulSoup(res.text,"lxml")
-        for a in soup.select('span.title a[href]'):
-            downloader_saavn(a['href'].strip())
-    else:
-        downloader_saavn(input_url)
+            soup = BeautifulSoup(res.text,"lxml")
+            for a in soup.select('span.title a[href]'):
+                downloader_saavn(a['href'].strip())
+        else:
+            downloader_saavn(input_url)
+
+        continueDownload = input('Do you want to continue downloading? (y/n):').strip()
+        if continueDownload == 'y':
+            loop = True
+        else:
+            loop = False
